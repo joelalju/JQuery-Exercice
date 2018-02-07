9c06ca47cc49983fdc7e83f9da48f1ef37f69ed6
joelalju <joelalju100@gmail.com>
2018-02-07 12:21:43 +0100

    Rotation added

--- jeje.js
+++ jeje.js
@@ -2,7 +2,7 @@ $(window).on("load", function() {
 	$("#foto").animate({
 		width: "100%",
 		height: "100%"
-	}, "slow", function() {
+	}, 2000, function() {
 			$("#foto").css({"background-image":"url(favicon.gif)"});
 		}
 	);
